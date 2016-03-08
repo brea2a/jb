d0799ddefafb3e884cad0388783aa5e6956f7ded
Frederic Cambus <fred@statdns.com>
2016-03-08 22:43:03 +0100

    Adding quotes to prevent word splitting, this fixes test failures with pdksh

--- tests.sh
+++ tests.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-[ $(./jo -a jo) = '["jo"]' ]	||
+[ "$(./jo -a jo)" = '["jo"]' ] ||
 	{ echo "Test 1: Jo logo failed" >&2; exit 1; }
-[ $(./jo n=7 a=Hello) = '{"n":7,"a":"Hello"}' ] ||
+[ "$(./jo n=7 a=Hello)" = '{"n":7,"a":"Hello"}' ] ||
 	{ echo "Test 2: Object failed" >&2; exit 2; }
