c234468e413fbdd7bb30188f1c62af4752c5f56e
Christopher Degawa <ccom@randomderp.com>
2021-04-30 14:54:58 -0500

    json: cast EFBFBD to char to silence warnings
    Signed-off-by: Christopher Degawa <ccom@randomderp.com>

--- json.c
+++ json.c
@@ -1225,9 +1225,9 @@ void emit_string(SB *out, const char *str)
 #endif
 						b += 6;
 					} else {
-						*b++ = 0xEF;
-						*b++ = 0xBF;
-						*b++ = 0xBD;
+						*b++ = (char)0xEF;
+						*b++ = (char)0xBF;
+						*b++ = (char)0xBD;
 					}
 					s++;
 				} else if (c < 0x1F || (c >= 0x80 && escape_unicode)) {
